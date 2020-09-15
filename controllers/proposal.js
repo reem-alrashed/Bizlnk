1cbc3fe8ac241c74d095daa493d1e51c45d77b1b
Reem <reem@Reems-MacBook-Pro-118.local>
2020-09-15 12:06:06 +0300

    Fixed proposal bug

--- controllers/proposal.js
+++ controllers/proposal.js
@@ -5,18 +5,34 @@ const { body, validationResult } = require('express-validator');
 
 
 module.exports={
- showProjectProposals : (req , res , next) =>
+ showProjectProposals : (req , res ) =>{
  Proposal.find({projectId:req.params.pid}).find().populate({path:'userId',model:User})
  .exec(function(err, proposals) 
  {
     res.locals.proposals = proposals 
      User.find({}).then(users =>{
          res.locals.users = users;
-         res.locals.u = 0;
          res.render('/proposal/listOfProposals')
-})}),
+})})
+ },
 submitProposal:(req,res,next)=>{
-    
+    const proposal = new Proposal({
+        userId:req.params.uid,
+       projectId:req.params.pid
+})
+   first.save(function (err) {
+       if (err){
+           req.flash('error','Error adding proposal');
+           res.render("project/search")
+           next(error);
+       }
+
+       else{ 
+           req.flash('success','تم تسليم العرض بنجاح');
+           res.render("project/search")
+           next() }
+
+     })
 }
 
 
--- routes/proposal.js
+++ routes/proposal.js
@@ -3,9 +3,8 @@ const proposalsControler =require('../controllers/proposal')
 const { body } = require('express-validator');
 
 
-router.get('/' , proposalsControler.showProposals)
-router.post('/' , proposalsControler.submitProposals)
-
+router.get('/:pid/show' , proposalsControler.showProposals)
+router.post('/:pid/:uid/submit' , proposalsControler.submitProposals)
 
 
 module.exports = router;
